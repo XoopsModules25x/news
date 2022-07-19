a420d6c4f17c000beb1a0cc39e8a7874a1253d7c
mambax7 <mambax7@gmail.com>
2022-07-19 07:07:06 -0400

    add include/common.php

--- submit.php
+++ submit.php
@@ -32,6 +32,9 @@ require_once __DIR__ . '/header.php';
 require_once XOOPS_ROOT_PATH . '/class/uploader.php';
 require_once XOOPS_ROOT_PATH . '/header.php';
 
+require_once __DIR__ . '/include/common.php';
+
+
 /** @var News\Helper $helper */
 $helper = News\Helper::getInstance();
 
