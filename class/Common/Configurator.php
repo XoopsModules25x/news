82dab7b064c25697c707448e308a1800cae5a8a4
mambax7 <mambax7@gmail.com>
2022-10-25 03:22:43 -0400

    Property is immediately rewritten

--- class/Common/Configurator.php
+++ class/Common/Configurator.php
@@ -47,7 +47,7 @@ class Configurator
         $config = require \dirname(__DIR__, 2) . '/config/config.php';
 
         $this->name            = $config->name;
-        $this->paths           = $config->paths;
+//        $this->paths           = $config->paths;
         $this->uploadFolders   = $config->uploadFolders;
         $this->copyBlankFiles  = $config->copyBlankFiles;
         $this->copyTestFolders = $config->copyTestFolders;
